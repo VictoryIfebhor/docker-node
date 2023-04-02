a0a5f24bbeb83dc71a15a9df2e65dc2e052ba32c
Victory Ifebhor <victory.ifebhor@gmail.com>
2023-04-02 01:02:26 +0100

    Changed configuration for redis connection.

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
-  REDIS_HOST: process.env.REDIS_URL || "redis",
-  REDIS_PORT: process.env.REDIS_PORT || 6379,
+  REDIS_URL: process.env.REDIS_URL,
   SESSION_SECRET: process.env.SESSION_SECRET,
 };
--- src/service/redis.js
+++ src/service/redis.js
@@ -2,9 +2,7 @@ const redis = require("redis");
 const RedisStore = require("connect-redis").default;
 const config = require("../config");
 
-const redisClient = redis.createClient({
-  host: config.REDIS_HOST,
-  port: config.REDIS_PORT,
-});
+const redisClient = redis.createClient({ url: config.REDIS_URL });
+redisClient.connect().catch((e) => console.log());
 
 module.exports.redisStore = new RedisStore({ client: redisClient });
